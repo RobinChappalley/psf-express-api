45ece3d591476b7bb90a6d99245f45d071f0b4ce
Robin Chappalley <robin@chapi.ch>
2025-12-24 15:13:01 +0100

    create first tests

--- spec/camps.spec.js
+++ spec/camps.spec.js
@@ -2,18 +2,18 @@ import supertest from "supertest";
 import app from "../app.js";
 
 describe("POST /users", function () {
-  it("should create a user", async function () {});
+  it("should create a user", async function () {
+    const res = await supertest(app)
+      .post("/users")
+      .send({
+        name: "John Doe",
+        password: "1234",
+      })
+      .expect(200)
+      .expect("Content-Type", /json/);
+  });
 });
 
 describe("GET /camps", function () {
   test.todo("should retrieve the list of camps");
 });
-
-const res = await supertest(app)
-  .post("/users")
-  .send({
-    name: "John Doe",
-    password: "1234",
-  })
-  .expect(200)
-  .expect("Content-Type", /json/);
