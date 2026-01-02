e16dea8caa9e25b29a00318936bedbc024349deb
Robin Chappalley <robin@chapi.ch>
2026-01-02 13:54:38 +0100

    Fixed test problems and db urls

---
+++ init-mongo.js
@@ -0,0 +1,14 @@
+const testdb = "psf-app-test";
+db = db.getSiblingDB("admin");
+db.createUser({
+  user: "test",
+  pwd: "rando",
+  roles: [
+    {
+      role: "dbOwner",
+      db: testdb,
+    },
+  ],
+});
+
+console.log("Les bases de données ont été initialisées !");
