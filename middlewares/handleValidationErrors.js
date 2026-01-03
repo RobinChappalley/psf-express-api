885b0e78807fba8b7397b85f37af8c76e95c9cd1
Robin Chappalley <robin@chapi.ch>
2026-01-03 16:22:44 +0100

    remove useless log

--- middlewares/handleValidationErrors.js
+++ middlewares/handleValidationErrors.js
@@ -2,7 +2,6 @@ import { validationResult } from "express-validator";
 
 const validateRequest = (req, res, next) => {
   const errors = validationResult(req);
-  console.log("Validation errors:");
   if (!errors.isEmpty()) {
     // Si express-validator a trouvé des erreurs, on arrête tout ici (400)
     return res.status(400).json({ errors: errors.array() });
