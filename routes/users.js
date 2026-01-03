88e2493fdc36c4d427a0d6ba61ee8646d73ba44f
joma0 <joanah.mayor@heig-vd.ch>
2026-01-03 17:29:14 +0100

    ajout des routes GET/users/:id PUT/users/:id et DELETE/users/:id

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,38 @@
 import express from "express";
-import { validateCreateUser } from "../validators/userValidator.js";
+import {
+  validateCreateUser,
+  validateUpdateUser,
+} from "../validators/userValidator.js";
 import validateRequest from "../middlewares/handleValidationErrors.js";
+import validateIdParam from "../validators/commonValidator.js";
 import UserController from "../controllers/userController.js";
 
 const router = express.Router();
 
-/**
- * POST /user
- * Ajouter un utilisateur
- */
 router.get("/", UserController.getAllUsers);
+
+router.get("/:id", validateIdParam, validateRequest, UserController.getOneUser);
 
 router.post(
   "/",
   validateCreateUser,
   validateRequest,
   UserController.createUser
+);
+
+router.put(
+  "/:id",
+  validateIdParam,
+  validateUpdateUser,
+  validateRequest,
+  UserController.updateUser
+);
+
+router.delete(
+  "/:id",
+  validateIdParam,
+  validateRequest,
+  UserController.deleteUser
 );
 
 export default router;
