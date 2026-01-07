3cb000c7a18ceedd56df9685c9f4c42d09e8db86
joma0 <joanah.mayor@heig-vd.ch>
2026-01-07 12:45:08 +0100

    correction validation email

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -1,12 +1,11 @@
 import { body } from "express-validator";
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
 export const validateLogin = [
   body("email")
     .notEmpty()
     .withMessage("Email is required")
-    .matches(emailRegex)
+    .isEmail()
+    .normalizeEmail()
     .withMessage("Email must contain @ and a domain with ."),
 
   body("password").notEmpty().withMessage("Password is required"),
