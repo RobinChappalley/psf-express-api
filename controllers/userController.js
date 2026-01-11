0e2b4529c918665ee91bb9e0cb591e97dd920fc9
Cedrine MacRempl <cedrine.tille@gmail.com>
2026-01-11 19:42:35 +0100

    Fix condition to check if user has paid

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,68 +1,66 @@
-import { matchedData } from "express-validator";
-import UserModel from "../models/User.model.js";
+import { matchedData } from 'express-validator'
+import UserModel from '../models/User.model.js'
 
 class UserController {
   async getAllUsers(req, res) {
-    const { parentId, campId, role, hasPaid } = req.query;
-    const filter = {};
+    const { parentId, campId, role, hasPaid } = req.query
+    const filter = {}
 
-    if (parentId) filter.parent = parentId;
-    if (campId) filter.camps = { $in: [campId] };
-    if (role) filter.role = role;
-    if (hasPaid != undefined) filter.hasPaid = hasPaid === true;
+    if (parentId) filter.parent = parentId
+    if (campId) filter.camps = { $in: [campId] }
+    if (role) filter.role = role
+    if (hasPaid !== undefined) filter['participationInfo.hasPaid'] = hasPaid === 'true'
 
-    const users = await UserModel.find(filter);
+    const users = await UserModel.find(filter)
 
-    res.status(200).json(users);
+    res.status(200).json(users)
   }
 
   async getOneUser(req, res) {
-    const user = await UserModel.findById(req.params.id);
+    const user = await UserModel.findById(req.params.id)
 
     if (!user) {
-      const error = new Error("User not found");
-      error.status = 404;
-      throw error;
+      const error = new Error('User not found')
+      error.status = 404
+      throw error
     }
 
-    res.status(200).json(user);
+    res.status(200).json(user)
   }
 
   async createUser(req, res) {
-    const data = matchedData(req);
-    const newUser = await UserModel.create(data);
-    res.status(201).json(newUser);
+    const data = matchedData(req)
+    const newUser = await UserModel.create(data)
+    res.status(201).json(newUser)
   }
 
   async updateUser(req, res) {
-    const data = matchedData(req);
+    const data = matchedData(req)
     const updatedUser = await UserModel.findByIdAndUpdate(req.params.id, data, {
       new: true,
-      runValidators: true,
-    });
+      runValidators: true
+    })
 
     if (!updatedUser) {
-      const error = new Error("User not found");
-      error.status = 404;
-      throw error;
+      const error = new Error('User not found')
+      error.status = 404
+      throw error
     }
 
-    res.status(200).json(updatedUser);
+    res.status(200).json(updatedUser)
   }
 
   async deleteUser(req, res) {
-    const deletedUser = await UserModel.findByIdAndDelete(req.params.id);
+    const deletedUser = await UserModel.findByIdAndDelete(req.params.id)
 
     if (!deletedUser) {
-      const error = new Error("User not found");
-      error.status = 404;
-      throw error;
+      const error = new Error('User not found')
+      error.status = 404
+      throw error
     }
 
-    res
-      .status(200)
-      .json({ message: "User successfully deleted", user: deletedUser });
+    res.status(200).json({ message: 'User successfully deleted', user: deletedUser })
   }
 }
 
-export default new UserController();
+export default new UserController()
